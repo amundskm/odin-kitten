c1db07e23d3b1514c4958c2df0ec362598e048cc
amundskm <amundskm@gmail.com>
2019-02-26 09:18:08 -0600

    add attributes to kitten model

--- db/migrate/20190226151418_create_kittens.rb
+++ db/migrate/20190226151418_create_kittens.rb
@@ -1,7 +1,10 @@
 class CreateKittens < ActiveRecord::Migration[5.2]
   def change
     create_table :kittens do |t|
-
+      t.text :name
+      t.integer :age
+      t.text :cuteness
+      t.text :softness
       t.timestamps
     end
   end
