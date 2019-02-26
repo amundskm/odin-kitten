e10b2da5f0ba30ccb157d5e5426846d3142c733e
amundskm <amundskm@gmail.com>
2019-02-26 09:35:47 -0600

    create basic kitten controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,36 @@
 class KittensController < ApplicationController
+  def new
+    @kitten = Kitten.new
+  end
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.create(kitten_params)
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten.update_attributes(kitten_params)
+  end
+
+  def destroy
+    @kitten.find(params[:id]).destroy
+  end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).premit(:name, :age, :cuteness, :softness)
+    end
+
 end
